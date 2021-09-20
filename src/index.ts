6e8250d14364a82b8db7acafaf1a0acce0ef5a2b
Aurélien Manteaux <amanteaux@coreoz.com>
2021-09-20 15:00:28 +0200

    export exportGlobalInjector

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Injector } from './lib/Injector';
 export { Provider } from './lib/Provider';
-export { configureGlobalInjector, getGlobalInstance } from './lib/InjectorInstance';
+export { configureGlobalInjector, getGlobalInstance, exportGlobalInjector } from './lib/InjectorInstance';
